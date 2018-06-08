bebf7cfd610432a269a5be4eeb0fe1c7a6f0e7e7
laiyy <laiyy0728@gmail.com>
2018-06-08 21:11:26 +0800

    layui包导入，初始化 index demo

--- src/main/java/com/laiyy/springquartz/controller/JobController.java
+++ src/main/java/com/laiyy/springquartz/controller/JobController.java
@@ -1,9 +1,15 @@
 package com.laiyy.springquartz.controller;
 
+import com.laiyy.springquartz.dto.AjaxDto;
+import com.laiyy.springquartz.model.Job;
 import com.laiyy.springquartz.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.transform.Result;
 
 /**
  * @author laiyy
@@ -17,7 +23,10 @@ public class JobController {
     @Autowired
     private JobService jobService;
 
-
+    @GetMapping
+    public Page<Job> list(int page){
+        return jobService.findJobByGroupId(1, page);
+    }
 
 
 }
